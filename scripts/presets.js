f89b937ba8a94274b16040d129ffa81e310d2ea4
en-programmerare <matdrys@gmail.com>
2022-10-18 12:02:16 +0200

    Ny skola: Arlandagymnasiet.

--- scripts/presets.js
+++ scripts/presets.js
@@ -1,41 +1,48 @@
 "use strict";
 
 const LovnedraknarePresets = {
-    schools: [{
+    schools: [
+		{
             id: "ostra",
             name: "Östra gymnasiet",
             image: "images/schools/ostra.jpg",
-            breaks: [{
+            breaks: [
+				{
                     id: "host",
                     startDate: "2022-10-28",
                     startTime: "16:00",
                     endDate: "2022-11-07",
                     endTime: "00:00"
-                }, {
+                }, 
+				{
                     id: "jul",
                     startDate: "2022-12-21",
                     startTime: "13:00",
                     endDate: "2023-01-10",
                     endTime: "00:00"
-                }, {
+                }, 
+				{
                     id: "sport",
                     startDate: "2023-02-24",
                     startTime: "16:00",
                     endDate: "2023-03-06",
                     endTime: "00:00"
-                }, {
+                }, 
+				{
                     id: "pask",
                     startDate: "2023-04-06",
                     startTime: "16:00",
                     endDate: "2023-04-17",
                     endTime: "00:00"
-                }, {
+                }, 
+				{
                     id: "sommar",
                     startDate: "2023-06-09",
                     startTime: "10:30",
                     endDate: "2023-08-10",
                     endTime: "00:00"
-                }, {
+                }, 
+				{
                     id: "student",
                     startDate: "2023-06-02",
                     startTime: "13:13",
@@ -43,73 +50,161 @@ const LovnedraknarePresets = {
                     endTime: "00:00"
                 }
             ],
-            classes: [{
+            classes: [
+				{
                     name: "TETE20",
                     id: "tete20",
-                    breaks: [{
+                    breaks: [
+						{
                             id: "host",
                             startTime: "12:35"
-                        }, {
+                        }, 
+						{
                             id: "jul"
-                        }, {
+                        }, 
+						{
                             id: "sport",
                             startTime: "12:35"
-                        }, {
+                        }, 
+						{
                             id: "pask",
                             startTime: "15:30"
-                        }, {
+                        }, 
+						{
                             id: "student"
                         }
                     ]
-                }, {
+                }, 
+				{
                     name: "NANASA20",
                     id: "nanab20",
-                    breaks: [{
+                    breaks: [
+						{
                             id: "host",
                             startTime: "14:15"
-                        }, {
+                        }, 
+						{
                             id: "jul"
-                        }, {
+                        }, 
+						{
                             id: "sport",
                             startTime: "14:15"
-                        }, {
+                        }, 
+						{
                             id: "pask",
                             startTime: "16:55"
-                        }, {
+                        }, 
+						{
                             id: "student"
                         }
                     ]
                 }
             ],
-            flags: [{
+            flags: [
+				{
                     name: "Indval IDH 2",
                     id: "idh2",
-                    breaks: [{
+                    breaks: [
+						{
                             id: "host",
                             startTime: "16:00"
-                        }, {
+                        }, 
+						{
                             id: "sport",
                             startTime: "16:00"
                         }
                     ]
-                }, {
+                }, 
+				{
                     name: "Indval japanska 2",
                     id: "jap2",
-                    breaks: [{
+                    breaks: [
+						{
                             id: "host",
                             startTime: "14:35"
-                        }, {
+                        }, 
+						{
                             id: "sport",
                             startTime: "14:35"
-                        }, {
+                        }, 
+						{
                             id: "pask",
                             startTime: "16:45"
                         }
                     ]
                 }
             ]
+        },
+		
+		
+		
+		
+		
+		{
+            id: "arlanda",
+            name: "Arlandagymnasiet",
+            image: "images/schools/arlanda.jpg",
+            breaks: [
+				{
+                    id: "host",
+                    startDate: "2022-10-28",
+                    startTime: "16:00",
+                    endDate: "2022-11-07",
+                    endTime: "00:00"
+                }, 
+				{
+                    id: "jul",
+                    startDate: "2022-12-21",
+                    startTime: "13:00",
+                    endDate: "2023-01-10",
+                    endTime: "00:00"
+                }, 
+				{
+                    id: "sport",
+                    startDate: "2023-02-24",
+                    startTime: "16:00",
+                    endDate: "2023-03-06",
+                    endTime: "00:00"
+                }, 
+				{
+                    id: "pask",
+                    startDate: "2023-04-06",
+                    startTime: "16:00",
+                    endDate: "2023-04-17",
+                    endTime: "00:00"
+                }, 
+				{
+                    id: "sommar",
+                    startDate: "2023-06-09",
+                    startTime: "10:30",
+                    endDate: "2023-08-10",
+                    endTime: "00:00"
+                }, 
+				{
+                    id: "student",
+                    startDate: "2023-06-02",
+                    startTime: "13:13",
+                    endDate: "2023-08-10",
+                    endTime: "00:00"
+                }
+            ],
+            classes: [
+				{
+                    name: "Fly20b",
+                    id: "fly20b",
+                    breaks: [
+						{
+                            id: "host",
+                            startTime: "12:30"
+                        }
+                    ]
+                }
+			]
         }
     ],
+
+
+
 
     breaks: [{
             id: "host",
